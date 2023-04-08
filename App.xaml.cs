b35ed9bb3c312e13120492fbc033930263a71114
JGMelon22 <jgvasconcellos22@gmail.com>
2023-04-08 15:10:28 -0300

    Preparing App.xaml to IoC logic/services

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,48 @@
-﻿using System.Windows;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Data;
+using System.IO;
+using System.Windows;
 
-namespace WpfDapperEfCoreDemo
+namespace WpfDapperEfCoreDemo;
+
+
+public partial class App : Application
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
+	public static IHost? AppHost { get; private set; }
+	public App() // Service registration
 	{
+		AppHost = Host.CreateDefaultBuilder()
+			.ConfigureAppConfiguration((hostContext, config) =>
+			{
+				config.SetBasePath(Directory.GetCurrentDirectory());
+				config.AddJsonFile(@"C:\Users\joao\Documents\Desenvolvimento\TempCode\WpfDapperEfCoreDemo\appsettings.json", optional: false, reloadOnChange: true);
+			})
+			.ConfigureServices((hostContext, services) =>
+			{
+				services.AddSingleton<MainWindow>();
+				services.AddScoped<IDbConnection>((x) => new SqlConnection(hostContext.Configuration.GetConnectionString("Default")));
+			})
+			.Build();
+	}
+
+	// Start services 
+	protected override async void OnStartup(StartupEventArgs e)
+	{
+		await AppHost!.StartAsync(); // Start application
+
+		var startUpForm = AppHost.Services.GetRequiredService<MainWindow>();
+		startUpForm.Show();
+
+		base.OnStartup(e);
+	}
+
+
+	protected override async void OnExit(ExitEventArgs e)
+	{
+		await AppHost!.StopAsync();
+		base.OnExit(e);
 	}
 }
